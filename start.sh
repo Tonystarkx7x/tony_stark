f38227b7fc1572b9133f6330049798ddcb0c2778
Tonystarkx7x <146725814+Tonystarkx7x@users.noreply.github.com>
2023-10-04 10:13:01 +0530

    Update start.sh

--- start.sh
+++ start.sh
@@ -1,10 +1,10 @@
 if [ -z $UPSTREAM_REPO ]
 then
   echo "Cloning main Repository"
-  git clone https://github.com/shahidalamkkbk/Advance-Auto-Filter /Advance-Auto-Filter
+  git clone https://github.com/Tonystarkx7x/tony_stark /tony_stark
 else
   echo "Cloning Custom Repo from $UPSTREAM_REPO "
-  git clone $UPSTREAM_REPO /Advance-Auto-Filter
+  git clone $UPSTREAM_REPO /tony_stark
 fi
 cd /Advance-Auto-Filter
 pip3 install -U -r requirements.txt
