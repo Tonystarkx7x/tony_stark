b29058fbcf35d8fef9e80eda18a093781b4ead79
Tonystarkx7x <146725814+Tonystarkx7x@users.noreply.github.com>
2023-10-10 18:24:10 +0530

    Update start.sh

--- start.sh
+++ start.sh
@@ -6,7 +6,7 @@ else
   echo "Cloning Custom Repo from $UPSTREAM_REPO "
   git clone $UPSTREAM_REPO /tony_stark
 fi
-cd /Advance-Auto-Filter
+cd /tony_stark
 pip3 install -U -r requirements.txt
 echo "Starting Bot...."
 python3 bot.py
